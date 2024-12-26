e47f7c3b1f8d387ae0434f963a051a6ba37d61af
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2024-12-26 13:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	if(arr.length===1)return 0;
+	else if(arr.length===2)return arr[0]+arr[1];
+	arr.sort();
+	let cost = 0;
+	let tmp=arr[0]+arr[1];
+	for(i=2;i<arr.length;++i){
+		cost = cost +tmp;
+		temp = tmp+arr[i];
+	}
+	return cost+tmp;
 }
 
 module.exports=mincost;
