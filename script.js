8bfa27557f68cd52941c5294aa87d20239eec045
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2024-12-26 14:13:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function mincost(arr)
 //write your code here
 // return the min cost
 	if(arr.length===1)return 0;
-	else if(arr.length===2)return arr[0]+arr[1];
-	arr.sort();
-	let cost = 0;
-	let tmp=arr[0]+arr[1];
-	for(i=2;i<arr.length;++i){
-		cost = cost +tmp;
-		tmp = tmp+arr[i];
+	let totalCost =0;
+	while(arr.length>1){
+		arr.sort();
+		let cost = arr[0]+arr[1];
+		totalCost+=cost;
+		arr = arr.slice(2);
+		arr.push(totalCost);
 	}
-	return cost+tmp;
+	return totalCost;
 }
 
 module.exports=mincost;
