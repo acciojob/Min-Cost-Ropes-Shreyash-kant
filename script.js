209b4616e352de546c58b9730ac34607a74d13c8
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2024-12-26 14:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function mincost(arr)
 	if(arr.length===1)return 0;
 	let totalCost =0;
 	while(arr.length>1){
-		arr.sort();
+		arr.sort((a,b)=>(a-b));
 		let cost = arr[0]+arr[1];
 		totalCost+=cost;
 		arr = arr.slice(2);
